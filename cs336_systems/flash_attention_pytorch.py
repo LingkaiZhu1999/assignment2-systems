20570abaf8d59c65daca6db0bb190bf44f02a37e
LingkaiZhu1999 <zhulingkai123@gmail.com>
2026-04-05 15:49:57 +0800

    flash attention forward pytorch implementation

---
+++ cs336_systems/flash_attention_pytorch.py
@@ -0,0 +1,49 @@
+import torch
+from einops import rearrange, einsum
+import math
+
+class FlashAttention(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, Q, K, V, is_causal=False):
+        # Q: [Batch, N_q, d], K, V: [Batch, N_k, d]
+        batch, N_q, d = Q.shape
+        batch, N_k, d = K.shape
+        B_q = 16
+        B_k = 16
+        tiled_Q = rearrange(Q, "... (num_tile B_q) d -> ... num_tile B_q d", B_q=B_q)
+        tiled_K = rearrange(K, "... (num_tile B_k) d -> ... num_tile B_k d", B_k=B_k)
+        tiled_V = rearrange(V, "... (num_tile B_k) d -> ... num_tile B_k d", B_k=B_k)
+        # split
+        T_q = torch.ceil(torch.div(N_q, B_q)).int()
+        T_k = torch.ceil(torch.div(N_k, B_k)).int()
+
+        O_full = torch.empty((batch, N_q, d), device=Q.device)
+        L_full = torch.empty((batch, N_q), device=Q.device)
+        for i in range(T_q):
+            Q_block = tiled_Q[:, i]
+            O = torch.zeros((batch, B_q, d), device=Q.device)
+            l = torch.zeros((batch, B_q, ), device=Q.device)
+            m = torch.full((batch, B_q, ), -torch.inf, device=Q.device)
+            
+            for j in range(T_k):
+                K_block, V_block = tiled_K[:, j], tiled_V[:, j]
+                S = einsum(Q_block, K_block, "... B_q d, ... B_k d -> ... B_q B_k") / math.sqrt(d)
+                m_prev = m.clone()
+                m = torch.maximum(m, torch.max(S, dim=-1).values)
+                P = torch.exp(S - rearrange(m, "... B_q -> ... B_q 1"))
+                l = torch.exp(m_prev - m) * l + torch.sum(P, dim=-1)
+                O = einsum(torch.diag_embed(torch.exp(m_prev - m)), O, "... B_q B_q, ... B_q d -> ... B_q d") + \
+                einsum(P, V_block, "... B_q B_k, ... B_k d -> ... B_q d")
+
+            O = einsum(torch.diag_embed(1./l), O, "... B_q B_q, ... B_q d -> ... B_q d")
+            L = m + torch.log(l)
+            O_full[:, i*B_q: B_q*(i+1), :] = O
+            L_full[:, i*B_q: B_q*(i+1)] = L
+        
+        ctx.save_for_backward(O_full, L_full)
+        
+        return O_full
+    
+    @staticmethod
+    def backward(ctx, *grad_outputs):
+        raise NotImplementedError
--- tests/adapters.py
+++ tests/adapters.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import Type
 
 import torch
-
+from cs336_systems.flash_attention_pytorch import FlashAttention
 
 
 def get_flashattention_autograd_function_pytorch() -> Type:
@@ -16,8 +16,7 @@ def get_flashattention_autograd_function_pytorch() -> Type:
         A class object (not an instance of the class)
     """
     # For example: return MyFlashAttnAutogradFunctionClass
-    raise NotImplementedError
-
+    return FlashAttention
 
 def get_flashattention_autograd_function_triton() -> Type:
     """
